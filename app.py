9012222741fa617d3a78dd6bb0ba93b49f03e254
drkh <abhinandnmurali@gmail.com>
2024-09-19 02:36:23 +0530

    Tried to add a filter at xss

--- app.py
+++ app.py
@@ -176,6 +176,7 @@ def product_details(pid):
         if 'document.cookie' in prod['description']:
             flash("<div class='alert alert-primary' role='alert'>Congrats! You've found the flag: $FLAG1#37f7c008e19a3e9ea2c82610ce7a36d6c0a3715d</div>")
             print("Hello flag")
+
         return render_template('product_details.html', product=prod)
     else:
         abort(404) 
